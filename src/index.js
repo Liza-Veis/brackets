bdb5f8566c9ccfdc1aaaef6acc6826d16ffe6657
Liza Veis <liza.veis14@gmail.com>
2020-10-07 00:23:31 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets = {};
+  for (let val of bracketsConfig) {
+    brackets[val[0]] = val[1];
+  }
+  let chars = str.split("");
+  for (let i = 0; i < chars.length - 1; i++) {
+    if (brackets[chars[i]] === chars[i + 1] && brackets[chars[i]] !== undefined) {
+      chars.splice(i, 2);
+      i -= 2;
+    }
+  }
+  return chars.join("") === "" ? true : false;
+};
